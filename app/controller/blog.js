daf3f1e0077e8983b70856ed18e91f463ab8ed3c
QITMAC000948 <1477869317@qq.com>
2019-10-30 10:12:00 +0800

    :sparkles:

--- app/controller/blog.js
+++ app/controller/blog.js
@@ -5,11 +5,6 @@ const Controller = require('egg').Controller;
 class BlogController extends Controller {
   async index() {
     const { ctx } = this;
-
-    // 获取仓库信息，目的获取issues的数量，便于分页
-    await ctx.curl('https://api.github.com/repos/strongcode9527/blog');
-    await ctx.curl('https://api.github.com/repos/strongcode9527/blog/issues', {});
-
     ctx.body = 'hi, egg';
   }
 }
---
+++ schedule/updateBlog.js
@@ -0,0 +1,22 @@
+'use strict';
+const Subscription = require('egg').Subscription;
+
+class UpdateBlog extends Subscription {
+  // 通过 schedule 属性来设置定时任务的执行间隔等配置
+  static get schedule() {
+    return {
+      interval: '10s', // 1 分钟间隔
+      type: 'worker', // 机器上随机一个worker会执行此定时任务
+    };
+  }
+
+  // subscribe 是真正定时任务执行时被运行的函数
+  async subscribe() {
+    console.log('in--------------');
+    // 获取仓库信息，目的获取issues的数量，便于分页
+    // const repo = await this.ctx.curl('https://api.github.com/repos/strongcode9527/blog');
+    // const blogs = await this.ctx.curl('https://api.github.com/repos/strongcode9527/blog/issues', {});
+  }
+}
+
+module.exports = UpdateBlog;
