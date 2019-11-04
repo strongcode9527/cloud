33dd22d67e1c4429e31140571d48eded8151a2ca
QITMAC000948 <1477869317@qq.com>
2019-11-04 10:34:45 +0800

    :sparkles:

--- app/lib/plugin/blog/app/schedule/updateBlog.js
+++ app/lib/plugin/blog/app/schedule/updateBlog.js
@@ -4,6 +4,7 @@ const path = require('path');
 const safeGetValue = require('../../../../../../utils/safeGetValue');
 const Subscription = require('egg').Subscription;
 const versionConfigPath = path.resolve(process.cwd(), './.blogs/__version__.json');
+const blogDataBaseUrl = path.resolve(process.cwd(), './.blogs');
 const token = [ 'd1e12b7fbb99bfc2a968803b2', '0d7854d7f9c8f98' ];
 
 class UpdateBlog extends Subscription {
@@ -33,20 +34,40 @@ class UpdateBlog extends Subscription {
   updateBlog(blogs) {
     blogs = JSON.parse(blogs.toString());
     const isExits = fs.existsSync(versionConfigPath);
+    // 初始化version文件
     if (!isExits) {
       const jsonData = {};
       blogs.forEach(blog => {
-        jsonData[blog.number] = {
-          updateTime: blog.updated_at,
-          body: blog.body,
-        };
+        jsonData[blog.number] = { updated_at: blog.updated_at, title: blog.title };
       });
-      console.log(jsonData);
+      this.writeFileSync(blogs);
       fs.writeFileSync(versionConfigPath, JSON.stringify(jsonData));
-    } else {
+    } else { // 更新version文件
       const versionFile = fs.readFileSync(versionConfigPath);
-      console.log(versionFile.toString());
+      const versionJson = JSON.parse(versionFile);
+      const updateBlogs = [];
+      blogs.forEach(blog => {
+        if (safeGetValue('', [ blog.number, 'updated_at' ], versionJson) !== blog.updated_at) {
+          console.log(safeGetValue('', [ blog.number ], versionJson), blog.updated_at)
+          versionJson[blog.number] = { updated_at: blog.updated_at, title: blog.title };
+          updateBlogs.push(blog);
+        }
+      });
+      if (updateBlogs.length) {
+        fs.writeFileSync(versionConfigPath, JSON.stringify(versionJson));
+        this.writeFileSync(updateBlogs);
+      }
     }
+  }
+  // 这里异步更新或创建博客文章
+  writeFileSync(blogs) {
+    blogs.forEach(blog => {
+      const obj = {
+        title: blog.title,
+        body: blog.body,
+      };
+      fs.writeFile(path.resolve(blogDataBaseUrl, `./${blog.number}.json`), JSON.stringify(obj), function() {});
+    });
   }
 }
 
--- app/lib/plugin/blog/lib/client.js
+++ app/lib/plugin/blog/lib/client.js
@@ -1,12 +1,23 @@
 'use strict';
 
 const Base = require('sdk-base');
-
+const fs = require('fs');
+cosnt path = require('path');
+const baseUrl = path.resolve(process.cwd(), './.blogs');
 class Client extends Base {
   constructor(options) {
     super(options);
     // 在初始化成功以后记得 ready
     console.log('in there');
+  }
+
+  get(id) {
+    try {
+      const data = fs.readFileSync(path.resolve(baseUrl, `./${id}.json`), 'utf-8');
+      return data
+    } catch(err) {
+      return '没有此博文';
+    }
   }
 
 }
